8643fcbf6d2a6a42d22a908e637210381b3f9590
rouf <rouf-linux@precision-3650-tower-rouf.rd.unr.edu>
2023-10-19 21:49:49 -0700

    added all data for cloud california

--- transfer_detection_sum/run.py
+++ transfer_detection_sum/run.py
@@ -13,24 +13,25 @@ models = ["yolov5", "ssd", "retinanet"]
 devices = ["cuda", "cpu"]
 
 for camera in cameras:
-    for instance in instances:
-        for model in models:
-            for device in devices:
-                print("processing ...", camera, instance, model, device)
-                #load transfer_csv
-                transfer_time_csv_path = source_data_path + camera + "/" + procedures[0] + "/" + instance + "/" + transfer_metric + ".csv"
-                detection_time_csv_path = source_data_path + camera + "/" + procedures[1] + "/" + instance + "/" + model + "/" + device + ".csv"
-                transfer_time_df = pandas.read_csv(transfer_time_csv_path)
-                detection_time_df = pandas.read_csv(detection_time_csv_path)
-                common_columns = transfer_time_df.columns[:2]
-                summed_data = transfer_time_df.drop(columns=common_columns) + detection_time_df.drop(columns=common_columns)
-                result_df = pandas.concat([transfer_time_df[common_columns], summed_data], axis=1)
+    transfer_instance = "cloud_california"
+    detection_instance = "cloud_virginia"
+    for model in models:
+        for device in devices:
+            print("processing ...", camera, transfer_instance, model, device)
+            #load transfer_csv
+            transfer_time_csv_path = source_data_path + camera + "/" + procedures[0] + "/" + transfer_instance + "/" + transfer_metric + ".csv"
+            detection_time_csv_path = source_data_path + camera + "/" + procedures[1] + "/" + detection_instance + "/" + model + "/" + device + ".csv"
+            transfer_time_df = pandas.read_csv(transfer_time_csv_path)
+            detection_time_df = pandas.read_csv(detection_time_csv_path)
+            common_columns = transfer_time_df.columns[:2]
+            summed_data = transfer_time_df.drop(columns=common_columns) + detection_time_df.drop(columns=common_columns)
+            result_df = pandas.concat([transfer_time_df[common_columns], summed_data], axis=1)
 
-                save_dir_path = result_path + camera + "/" + instance + "/" + model + "/"
-                filename = device + ".csv"
+            save_dir_path = result_path + camera + "/" + transfer_instance + "/" + model + "/"
+            filename = device + ".csv"
 
-                os.makedirs(save_dir_path, exist_ok=True)
-                result_df.to_csv(save_dir_path + filename, index=False)
+            os.makedirs(save_dir_path, exist_ok=True)
+            result_df.to_csv(save_dir_path + filename, index=False)
 
 
                 
