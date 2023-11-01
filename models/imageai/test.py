66c584ea6dd4b757203e2565ce2d9196e8692e54
Ubuntu <ubuntu@ip-172-31-75-23.ec2.internal>
2023-11-01 17:56:55 +0000

    refactored repo

--- models/imageai/test.py
+++ models/imageai/test.py
@@ -1,15 +1,16 @@
 from imageai.Detection import ObjectDetection
 from time import time
 
+home_directory = "/home/ubuntu/"
 detector = ObjectDetection()
 # detector.useCPU()
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath("~/retinanet_model.pth")  # Specify the path to the RetinaNet model
+detector.setModelPath(home_directory + "retinanet_model.pth")  # Specify the path to the RetinaNet model
 detector.loadModel()
 
 # Detect objects in an image
 start_time = time() * 1000
-detections = detector.detectObjectsFromImage(input_image="im1.png")
+detections = detector.detectObjectsFromImage(input_image= home_directory + "coco128/images/train2017/000000000034.jpg")
 end_time = time() * 1000
 
 print(end_time - start_time)
