3c31e4e8e73f25544d349b62531bc9ef02327ada
roufunr <rouf@nevada.unr.edu>
2023-11-04 11:52:21 -0700

    Client code and image transfer result added

--- image_transfer/client/run_client_over_http.py
+++ image_transfer/client/run_client_over_http.py
@@ -4,11 +4,31 @@ import time
 from datetime import datetime  # Import datetime module
 
 # Specify the URL of your Flask server
-ip_addr = '54.67.31.203'
+ip_addr = '44.203.150.47'
 upload_url = f"http://{ip_addr}:5000/upload"  # Use an f-string
 
-data_path = "/home/rouf-linux/data/data_32"
-result_path = "/home/rouf-linux/edge-computing/models/uploader_client/result/cloud_virginia"
+data_path = "/home/rouf-linux/data"
+compressed_data_path = data_path + "/compressed"
+original_data_path = data_path + "/original"
+result_path = "/home/rouf-linux/edge-computing/image_transfer/client/result"
+
+resolutions = ['160x90', '160x100', '160x120', '320x180', '320x200', '320x240', '480x270', '480x300', '480x360', '640x360', '640x400', '640x480', '800x450', '800x500', '800x600', '1024x576', '1024x640', '1024x768', '1280x720', '1280x800', '1280x960', '1440x900', '1440x1080', '1920x1080']
+frames = [1, 2, 4, 8, 16, 32]
+total_experiment = 11
+scaling_factors = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+ip_methods = ['INTER_NEAREST', 'INTER_LINEAR', 'INTER_CUBIC', 'INTER_AREA', 'INTER_LANCZOS4']
+ip_methods_name_mapper = {
+    'INTER_NEAREST': "Nearest",
+    'INTER_LINEAR': "Linear",
+    'INTER_CUBIC': "Inter-Cubic",
+    'INTER_AREA': "Area",
+    'INTER_LANCZOS4': "Lanczos4"
+}
+
+
+def save_data_as_json(data, json_path):
+    with open(json_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
 
 
 def delete_all_data_on_server(): 
@@ -24,52 +44,78 @@ def delete_all_data_on_server():
         print('An error occurred:', str(e))
 
 
-        
-with open(data_path + '/labels.json', 'r') as json_file:
-    labels = json.load(json_file)
-
-modified_labels = {}
-for label in labels:
-    path = label['image_path']
-    image_name = path.split("/")[-1]
-    dir_path = path.replace(image_name, "")
-    if dir_path in modified_labels:
-        modified_labels[dir_path].append(image_name)
-    else:
-        modified_labels[dir_path] = [image_name]
-
-for i in range(10+1): 
-    total_data = len(labels)
+def transfer_compressed_image(exp_id):
+    total_data = len(ip_methods) * len(scaling_factors) * len(resolutions) * ((2**6) - 1)
     completed_data = 0
     results = {}
-    for key in modified_labels:
-        image_files = []
-        images_path = key
-        for image_name in modified_labels[key]:
-            image_files.append(("images", (image_name, open(data_path + key + image_name, 'rb'))))
-        start_time = time.time() * 1000
-        data = {'path': images_path, 'transfer_start_time': start_time}
-        response = requests.post(upload_url, files=image_files, data=data)  # Simplify request
-        end_time = time.time() * 1000
-        response_time = end_time - start_time
-        response_data = response.json()
-        results[key] = {
-            "transfer_time": response_data["transfer_time"],
-            "disk_write_time": response_data["disk_write_time"],
-            "response_time": response_time
-        }
-        completed_data += len(image_files)
-        print(str(i)  + "-> ""DONE", ((completed_data / total_data) * 100), "%")
     
-    json_results = json.dumps(results, indent=2)
-    date_time = datetime.utcfromtimestamp(time.time()).strftime('%Y%m%d_%H:%M:%S_utc')
-    result_json_name = f'exp_{i}_client_side_transfer_time_{date_time}.json'  # Use an f-string
-    with open(result_path + '/' + result_json_name, 'w') as json_file:
-        json_file.write(json_results)
-    
-    print("client goes to sleep for 30 sec!")
-    time.sleep(10)
-    delete_all_data_on_server()
-    time.sleep(20)
+    for ip_method in ip_methods:
+        for scaling_factor in scaling_factors:
+            for resolution in resolutions:
+                for frame in frames:
+                    first_image_idx = frame - 1
+                    image_files = []
+                    image_relative_path = ip_method + "/" + str(scaling_factor) + "/" + resolution + "/" + str(frame)
+                    for i in range(frame):
+                        image_name = str(first_image_idx + i) + ".bmp"
+                        image_absolute_path = compressed_data_path + "/" + image_relative_path + "/" + image_name
+                        image_files.append(("images", (image_name, open(image_absolute_path, 'rb'))))
+                    start_time = time.time() * 1000
+                    key = str(exp_id) + "/compressed/" +  ip_method + "/" + str(scaling_factor) + "/" + resolution + "/" + str(frame)
+                    data = {'path': key, 'transfer_start_time': start_time}
+                    response = requests.post(upload_url, files=image_files, data=data)  # Simplify request
+                    end_time = time.time() * 1000
+                    response_time = end_time - start_time
+                    response_data = response.json()
+                    results[key] = {
+                        "transfer_time": response_data["transfer_time"],
+                        "disk_write_time": response_data["disk_write_time"],
+                        "response_time": response_time
+                    }
+                    completed_data += len(image_files)
+                    print(str(exp_id)  + "-> " + "Compressed image transfer DONE", ((completed_data / total_data) * 100), "%")
+    return results
 
+def transfer_original_image(exp_id):
+    total_data = len(resolutions) * ((2**6) - 1)
+    completed_data = 0
+    results = {}
+    
+    
+    for resolution in resolutions:
+        for frame in frames:
+            first_image_idx = frame - 1
+            image_files = []
+            image_relative_path = resolution + "/" + str(frame)
+            for i in range(frame):
+                image_name = str(first_image_idx + i) + ".bmp"
+                image_absolute_path = original_data_path + "/" + image_relative_path + "/" + image_name
+                image_files.append(("images", (image_name, open(image_absolute_path, 'rb'))))
+            start_time = time.time() * 1000
+            key = str(exp_id) + "/original/" +  resolution + "/" + str(frame)
+            data = {'path': key, 'transfer_start_time': start_time}
+            response = requests.post(upload_url, files=image_files, data=data)  # Simplify request
+            end_time = time.time() * 1000
+            response_time = end_time - start_time
+            response_data = response.json()
+            results[key] = {
+                "transfer_time": response_data["transfer_time"],
+                "disk_write_time": response_data["disk_write_time"],
+                "response_time": response_time
+            }
+            completed_data += len(image_files)
+            print(str(exp_id)  + "-> ""DONE", ((completed_data / total_data) * 100), "%")
+    return results
+
+
+
+for i in range(total_experiment): 
+    compressed_result = transfer_compressed_image(i)
+    origianl_result = transfer_original_image(i)
+    result = {
+        "compressed": compressed_result,
+        "original": origianl_result
+    }
+
+    save_data_as_json(result, "/home/rouf-linux/edge-computing/image_transfer/client/result/" + str(i) + ".json")
 
