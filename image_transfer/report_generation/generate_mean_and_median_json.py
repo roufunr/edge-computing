cc3f945587b6a142f36b9730a687ecdcfcda8ffd
roufunr <rouf@nevada.unr.edu>
2023-11-05 02:01:17 -0800

    generated median and average and vertical data

--- image_transfer/report_generation/generate_mean_and_median_json.py
+++ image_transfer/report_generation/generate_mean_and_median_json.py
@@ -4,6 +4,7 @@ import json
 raw_results_base_path = "/Users/abdurrouf/edge-computing/image_transfer/report_generation/raw_results"
 parts = ["compression", "decompression", "transfer"]
 transfer_image_type = ["original", "compressed"]
+transfer_time_type = ["transfer_time", "disk_write_time", "response_time"]
 resolutions = ['160x90', '160x100', '160x120', '320x180', '320x200', '320x240', '480x270', '480x300', '480x360', '640x360', '640x400', '640x480', '800x450', '800x500', '800x600', '1024x576', '1024x640', '1024x768', '1280x720', '1280x800', '1280x960', '1440x900', '1440x1080', '1920x1080']
 frames = [1, 2, 4, 8, 16, 32]
 total_experiment = 10
@@ -26,18 +27,18 @@ def load_json(json_path):
         json_data = json.load(json_file)
     return json_data
 
-def generateKeys(exp_id, image_type):
+def generateKeys(image_type):
     keys = []
     if image_type == "original":
         for resolution in resolutions:
             for frame in frames:
-                keys.append(str(exp_id) + "/" + resolution + "/" + frame)
+                keys.append(resolution + "/" + str(frame))
     else: 
         for ip_method in ip_methods:
             for scaling_factor in scaling_factors:
                 for resolution in resolutions:
                     for frame in frames:
-                        keys.append(str(exp_id) + "/" + ip_method + "/" + str(scaling_factor) + "/" + resolution + "/" + str(frame))
+                        keys.append(ip_method + "/" + str(scaling_factor) + "/" + resolution + "/" + str(frame))
     return keys
 
 
@@ -56,9 +57,95 @@ def rearrange_data_vertically(data):
     v_data = {}
     for part in parts:
         if part == "transfer":
-            v_keys = {
-                "original": 
+            v_data["transfer"] = {
+                "original": {key:{timeKey:[] for timeKey in transfer_time_type} for key in generateKeys("original")},
+                "compressed": {key:{timeKey:[] for timeKey in transfer_time_type} for key in generateKeys("compressed")}
             }
+            
+            for image_type in transfer_image_type:
+                keys = v_data["transfer"][image_type]
+                for key in keys:
+                    for timeKey in transfer_time_type:
+                        for i in range(total_experiment):
+                            v_data["transfer"][image_type][key][timeKey].append(data["transfer"][i][image_type][key][timeKey])      
+        else: 
+            print(part)
+            v_data[part] = {key:[] for key in generateKeys("compressed")}
+            keys = v_data[part]
+            for key in keys:
+                for i in range(total_experiment):
+                    v_data[part][key].append(data[part][i][key])
+    return v_data
+
+def calculate_average(v_data): 
+    avg_data = {}
+    for part in parts:
+        if part == "transfer":
+            avg_data["transfer"] = {
+                "original": {key:{timeKey: 0 for timeKey in transfer_time_type} for key in generateKeys("original")},
+                "compressed": {key:{timeKey: 0 for timeKey in transfer_time_type} for key in generateKeys("compressed")}
+            }
+            
+            for image_type in transfer_image_type:
+                keys = v_data["transfer"][image_type]
+                for key in keys:
+                    for timeKey in transfer_time_type:
+                        sum = 0
+                        for i in range(total_experiment):
+                            sum += v_data["transfer"][image_type][key][timeKey][i]
+                        avg_data["transfer"][image_type][key][timeKey] = sum / total_experiment 
+        else:
+            avg_data[part] = {key: 0 for key in generateKeys("compressed")}
+            keys = v_data[part]
+            for key in keys:
+                sum = 0
+                for i in range(total_experiment):
+                    sum += v_data[part][key][i]
+                avg_data[part][key] = sum / total_experiment
+    return avg_data 
+
+def get_median(lst):
+    sorted_lst = sorted(lst)
+    n = len(sorted_lst)
+    if n % 2 == 0:
+        mid1 = sorted_lst[n // 2 - 1]
+        mid2 = sorted_lst[n // 2]
+        median = (mid1 + mid2) / 2.0
+    else:
+        median = sorted_lst[n // 2]
+    return median
+
+    
+def calculate_median(v_data): 
+    median_data = {}
+    for part in parts:
+        if part == "transfer":
+            median_data["transfer"] = {
+                "original": {key:{timeKey: 0 for timeKey in transfer_time_type} for key in generateKeys("original")},
+                "compressed": {key:{timeKey: 0 for timeKey in transfer_time_type} for key in generateKeys("compressed")}
+            }
+            
+            for image_type in transfer_image_type:
+                keys = v_data["transfer"][image_type]
+                for key in keys:
+                    for timeKey in transfer_time_type:
+                        median_data["transfer"][image_type][key][timeKey] = get_median(v_data["transfer"][image_type][key][timeKey])
+        else:
+            median_data[part] = {key: 0 for key in generateKeys("compressed")}
+            keys = median_data[part]
+            for key in keys:
+                median_data[part][key] = get_median(v_data[part][key])
+    return median_data 
+
+
+data = load_data()
+save_data_as_json(data, "/Users/abdurrouf/edge-computing/image_transfer/report_generation/raw_results/data.json")
+v_data = rearrange_data_vertically(data)
+save_data_as_json(v_data, "/Users/abdurrouf/edge-computing/image_transfer/report_generation/raw_results/v_data.json")
+avg_data = calculate_average(v_data)
+save_data_as_json(avg_data, "/Users/abdurrouf/edge-computing/image_transfer/report_generation/raw_results/avg_data.json")
+median_data = calculate_median(v_data)
+save_data_as_json(median_data, "/Users/abdurrouf/edge-computing/image_transfer/report_generation/raw_results/median_data.json")
 
         
 
