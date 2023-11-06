dfcb238419232c084b900b610889a5a22fcbb767
roufunr <rouf@nevada.unr.edu>
2023-11-06 14:55:09 -0800

    csv added

--- image_transfer/report_generation/deliverable.py
+++ image_transfer/report_generation/deliverable.py
@@ -18,25 +18,22 @@ ip_methods_name_mapper = {
 }
 
 
-def merge_csv_files(csv_file_paths, output_file_path):
+def merge_csv_files_with_empty_column(csv_file_paths, output_file_path):
     try:
-        # Create an empty DataFrame to store the merged data
         merged_data = pd.DataFrame()
 
-        for file_path in csv_file_paths:
-            # Read each CSV file into a DataFrame
+        for i, file_path in enumerate(csv_file_paths):
             df = pd.read_csv(file_path)
 
-            # Append the data with an empty column
             if not merged_data.empty:
-                merged_data[''] = ''  # Add an empty column
+                # Add an empty column before merging, except for the first file
+                merged_data[''] = ''
                 merged_data = pd.concat([merged_data, df], axis=1)
             else:
                 merged_data = df
 
-        # Save the merged data to a new CSV file
         merged_data.to_csv(output_file_path, index=False)
-        print(f"CSV files have been successfully merged and saved to '{output_file_path}'.")
+        print(f"CSV files have been successfully merged with empty columns and saved to '{output_file_path}'.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -45,21 +42,21 @@ csv_list = []
 for ip_method in ip_methods:
     for scaling_factor in scaling_factors:
         csv_list.append("/Users/abdurrouf/edge-computing/image_transfer/report_generation/csv/transfer/compressed" + "/" + ip_method + "/" + str(scaling_factor) + ".csv")
-output_csv_path = "/Users/abdurrouf/edge-computing/image_transfer/report_generation/deliverable" + "/" + "compressed_image_transfer.csv"
-merge_csv_files(csv_list, output_csv_path)
+output_csv_path = "/Users/abdurrouf/edge-computing/image_transfer/report_generation/deliverable_csv" + "/" + "compressed_image_transfer.csv"
+merge_csv_files_with_empty_column(csv_list, output_csv_path)
 
 # compression
 csv_list = []
 for ip_method in ip_methods:
     for scaling_factor in scaling_factors:
         csv_list.append("/Users/abdurrouf/edge-computing/image_transfer/report_generation/csv/compression" + "/" + ip_method + "/" + str(scaling_factor) + ".csv")
-output_csv_path = "/Users/abdurrouf/edge-computing/image_transfer/report_generation/deliverable" + "/" + "image_compression_time.csv"
-merge_csv_files(csv_list, output_csv_path)
+output_csv_path = "/Users/abdurrouf/edge-computing/image_transfer/report_generation/deliverable_csv" + "/" + "image_compression_time.csv"
+merge_csv_files_with_empty_column(csv_list, output_csv_path)
 
 # decompression
 csv_list = []
 for ip_method in ip_methods:
     for scaling_factor in scaling_factors:
         csv_list.append("/Users/abdurrouf/edge-computing/image_transfer/report_generation/csv/decompression" + "/" + ip_method + "/" + str(scaling_factor) + ".csv")
-output_csv_path = "/Users/abdurrouf/edge-computing/image_transfer/report_generation/deliverable" + "/" + "image_decompression_time.csv"
-merge_csv_files(csv_list, output_csv_path)
+output_csv_path = "/Users/abdurrouf/edge-computing/image_transfer/report_generation/deliverable_csv" + "/" + "image_decompression_time.csv"
+merge_csv_files_with_empty_column(csv_list, output_csv_path)
