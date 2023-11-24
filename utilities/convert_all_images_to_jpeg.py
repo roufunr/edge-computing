b25a41000304ebbed5365560ece085943651e313
roufunr <rouf@nevada.unr.edu>
2023-11-24 02:42:43 -0800

    bmp to jpeg conversion code added

---
+++ utilities/convert_all_images_to_jpeg.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+resolutions = {
+    "cam1": ['160x90', '160x100', '160x120', '240x180', '320x180', '320x200', '320x240', '480x270', '480x300', '480x360', '640x360', '640x400', '640x480', '800x450', '800x500', '800x600', '1024x576', '1024x640', '1024x768', '1280x720', '1280x800', '1280x960', '1440x900', '1440x1080', '1920x1080'],
+    "cam2": ['160x90', '160x100', '160x120', '240x180', '320x180', '320x200', '320x240', '480x270', '480x300', '480x360', '640x360', '640x400', '640x480', '800x450', '800x500', '800x600', '1024x576', '1024x640', '1024x768', '1280x720', '1280x800', '1280x960', '1440x900', '1440x1080', '1920x1080'],
+    "cam3": ['160x90', '160x120', '176x144', '240x180', '320x180', '320x240', '480x270', '480x360', '640x360', '640x480', '800x450', '800x600', '1024x640', '1024x768', '1280x720', '1280x800', '1280x960'],
+}
+
+def convert_to_jpeg(path):
+    input_image_path = path + "/0.bmp"
+    output_image_path = path + "/0.jpeg"
+    img = Image.open(input_image_path)
+    img.save(output_image_path, "JPEG")
+    
+for cam in resolutions:
+    for resolution in resolutions[cam]:
+        path = "/home/jtx2/data/original" + cam + "/" + resolution
+        convert_to_jpeg(path)
+        
